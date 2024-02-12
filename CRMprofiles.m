37e6137a2aae7b9dab5c6ca5782eaeaba60fed5a
Peter N. Adams <geopete1@gmail.com>
2024-02-12 15:36:38 -0500

    Add files via upload

---
+++ CRMprofiles.m
@@ -0,0 +1,31 @@
+close all,clearvars
+
+% Data obtained from:
+% https://floridadep.gov/rcp/beach-survey-services/content/historic-shoreline-database
+
+set_pna_colors;
+
+crmNumStr='57';
+dname='/Users/pna/Dropbox (UFL)/Data/Data06_BchPrfls/FlaDEP_ProfileSurveyData/UnZipped_ByCounty/STJPZ/COUNTYWIDE (R001-R209)/';
+
+fns={'SJ7209_CCC_1.PRF','SJ8405_CON_1.PRF','SJ9307_CON_1.PRF','SJ9902_CON_1.PRF',...
+    'SJ0306_CON_1.PRF','SJ0709_CON_1.PRF','SJ1105_CON_1.PRF','SJ1709_COE_1_mod.LID',...
+    'SJ1911_CON_1.PRF'};
+clrs={'brown','brick','red','magenta','purple','blue','cyan','hunter','green','spring'};
+for ii=1:length(fns)
+    fname=fns{ii};
+    [D{ii}]=ReadCRMprofiles_2023(dname,fname,crmNumStr);
+    subplot(2,1,1)
+    plot(D{ii}.x,D{ii}.z,'color',eval(['color.' clrs{ii}]),'linewidth',1.5),hold on
+    subplot(2,1,2)
+    plot(D{ii}.x,D{ii}.z,'color',eval(['color.' clrs{ii}]),'linewidth',1.5),hold on
+    set(gca,'XLim',[-200 600])
+end
+
+subplot(2,1,1),grid on
+set(gca,'FontSize',14),title(['Profile at R-' crmNumStr])
+ylabel('Elevation (ft)')
+legend('1972','1983','1993','1999','2003','2007','2011','2017','2019')
+subplot(2,1,2),grid on
+set(gca,'FontSize',14),title(['Dune and Intertidal Zone View'])
+xlabel('Cross Shore Position (ft)'),ylabel('Elevation (ft)')
---
+++ ReadCRMprofiles_2023.m
@@ -0,0 +1,45 @@
+function [D]=ReadCRMprofiles_2023(dname,fname,crmNumStr)
+
+% example call:
+% dname='/Users/pna/Dropbox (UFL)/Data/Data06_BchPrfls/STJPZ/COUNTYWIDE (R001-R209)/';
+% fname='SJ9902_CON_1.PRF';
+% crmNumStr='195';
+% [D]=TestReadCRMprofiles_2023(dname,fname,crmNumStr)
+fid=fopen([dname fname]);
+j=0;
+% D.exist='yes';
+while ~feof(fid)
+    L=fgetl(fid);
+    TF=contains(L,['R-' crmNumStr]);
+    RF=contains(L,['T-' crmNumStr]);
+    j=j+1;
+    SF=TF+RF;
+    if SF>=1
+%         disp(['found it: ' fname ' ' crmNumStr])
+%         if strcmp(fname,'SJ1709_COE_1_mod.LID'),L,end
+        D.RMid=L(1:5); D.RMdate=L(9:15); 
+        D.RMnorthingSPft=L(20:30); D.RMeastingSPft=L(32:42);
+        D.RMazimuth=L(44:49); D.RMelev=L(50:54);
+        [D.RM_lon,D.RM_lat]=sp_proj('florida east','inverse',...
+            str2num(D.RMeastingSPft),str2num(D.RMnorthingSPft),'sf');
+        [D.RM_utmE,D.RM_utmN,D.RM_utmZone]=deg2utm(D.RM_lat,D.RM_lon);
+        L=fgetl(fid);
+        D.SurvDateOnshore=L(9:15); D.SurvDateOffshore=L(17:23);
+        D.SurvNumDataPtsTotal=L(25:28);
+        D.SurvNumDataPtsOnshore=L(31:33);
+        D.SurvNumDataPtsOffshore=L(34:36);
+        N=[];
+        for k=1:ceil(str2num(D.SurvNumDataPtsTotal)/5)
+            L=fgetl(fid);
+            L(isletter(L))=[];
+            N=[N str2num(L)];
+            x=N(1:2:end);
+            z=N(2:2:end);
+        end
+        D.x=x;
+        D.z=z;
+        clear x z N
+    end
+end
+fclose(fid);
+
